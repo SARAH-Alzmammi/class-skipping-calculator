a3e3b4dd2c1fce8760eeb2019e67ab465010afc2
Sarah Alzmammi <s.zmammi@outlook.com>
2023-09-02 23:49:14 +0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        purple:'#7C81C4',
+        black:'#20201F',
+        yellow:'#F0DF6C',
       },
     },
   },
